f0dc5c704a261c9fcd9f4f35316df5cc92948970
Zainul <zainulbasharnazmin@gmail.com>
2023-06-17 22:58:23 +0530

    first-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[count,setCount] = useState(0);
+  const countClick = () =>{
+    setCount(count + 1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <button onClick={countClick}/>
+        <p>Button clicked {count} times</p>
+        
     </div>
   )
 }
